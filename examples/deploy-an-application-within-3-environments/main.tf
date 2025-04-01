789f8db3c0651a09ee9afb60e67284d3db51963a
Subham Kundu <152807348+htcd-subham@users.noreply.github.com>
2025-04-01 19:29:48 +0530

    Update examples/deploy-an-application-within-3-environments/main.tf

--- examples/deploy-an-application-within-3-environments/main.tf
+++ examples/deploy-an-application-within-3-environments/main.tf
@@ -76,99 +76,60 @@ resource "qovery_database" "production_psql_database" {
   accessibility  = "PRIVATE" # do not make it publicly accessible
 }
 
-resource "qovery_application" "production_strapi_app" {
+resource "qovery_application" "backend" {
   environment_id = qovery_environment.production.id
-  name           = "strapi app"
-  cpu            = 1000
-  memory         = 512
+  name           = "url-shortener-backend"  # More descriptive name
+  cpu            = 500
+  memory         = 256
+
   git_repository = {
-    url       = "https://github.com/evoxmusic/strapi-v4.git"
+    url       = "https://github.com/evoxmusic/ShortMe-URL-Shortener.git"
     branch    = "main"
     root_path = "/"
   }
+
   build_mode            = "DOCKER"
   dockerfile_path       = "Dockerfile"
   min_running_instances = 1
   max_running_instances = 1
-  ports                 = [
+
+  custom_domains = [
     {
-      internal_port       = 1337
+      domain = var.qovery_custom_domain
+    }
+  ]
+
+  ports = [
+    {
+      internal_port       = local.app_port
       external_port       = 443
       protocol            = "HTTP"
       publicly_accessible = true
       is_default          = true
     }
   ]
-  healthchecks = {
-    readiness_probe = {
-      type = {
-        http = {
-          port = 1337
-        }
-      }
-      initial_delay_seconds = 30
-      period_seconds        = 10
-      timeout_seconds       = 10
-      success_threshold     = 1
-      failure_threshold     = 3
-    }
-    liveness_probe = {
-      type = {
-        http = {
-          port = 1337
-        }
-      }
-      initial_delay_seconds = 30
-      period_seconds        = 10
-      timeout_seconds       = 10
-      success_threshold     = 1
-      failure_threshold     = 3
-    }
-  }
+
+  # Use environment variable block for better organization
   environment_variables = [
     {
-      key   = "PORT"
-      value = "1337"
+      key   = "DEBUG"
+      value = "false"
     },
     {
-      key   = "HOST"
-      value = "0.0.0.0"
-    },
-    {
-      key   = "DATABASE_HOST"
-      value = qovery_database.production_psql_database.internal_host
-    },
-    {
-      key   = "DATABASE_PORT"
-      value = qovery_database.production_psql_database.port
-    },
-    {
-      key   = "DATABASE_USERNAME"
-      value = qovery_database.production_psql_database.login
-    },
-    {
-      key   = "DATABASE_NAME"
-      value = "postgres"
-    },
-  ]
-  secrets = [
-    {
-      key   = "ADMIN_JWT_SECRET"
-      value = var.strapi_admin_jwt_secret
-    },
-    {
-      key   = "API_TOKEN_SALT"
-      value = var.strapi_api_token_salt
-    },
-    {
-      key   = "APP_KEYS"
-      value = var.strapi_app_keys
-    },
-    {
-      key   = "DATABASE_PASSWORD"
-      value = qovery_database.production_psql_database.password
+      key   = "APP_ENV"
+      value = "production"
     }
   ]
+
+  # Use the local variable for health checks to avoid repetition
+  healthchecks = {
+    readiness_probe = local.health_check_config
+    liveness_probe  = local.health_check_config
+  }
+
+  # Add auto-restart policy
+  auto_preview = false
+  auto_deploy  = true
 }
 
 resource "qovery_deployment" "prod_deployment" {
