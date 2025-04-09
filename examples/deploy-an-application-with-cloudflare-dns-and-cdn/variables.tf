d650c941adf98972340d8d5d790a4634bf928079
Subham Kundu <152807348+htcd-subham@users.noreply.github.com>
2025-04-09 17:13:44 +0530

    Update examples/deploy-an-application-with-cloudflare-dns-and-cdn/variables.tf

--- examples/deploy-an-application-with-cloudflare-dns-and-cdn/variables.tf
+++ examples/deploy-an-application-with-cloudflare-dns-and-cdn/variables.tf
@@ -1,37 +1,77 @@
-variable "qovery_organization_id" {
-  type    = string
+###
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
 }
 
-variable "qovery_access_token" {
-  type = string
+provider "aws" {
+  region = "us-east-1"
 }
 
-variable "aws_access_key_id" {
-  type = string
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+  default     = "vpc-0ddd932ce1bd038f6"
 }
 
-variable "aws_secret_access_key" {
-  type = string
+variable "security_group_id" {
+  description = "ID of the security group to be updated"
+  type        = string
+  default     = "sg-08fe4af16da156c0e"
 }
 
-variable "cloudflare_email" {
-  type = string
+variable "allowed_cidr_blocks" {
+  description = "List of allowed CIDR blocks for inbound traffic"
+  type        = list(string)
+  default     = ["10.0.0.0/16"] # Replace with your actual allowed CIDR blocks
 }
 
-variable "cloudflare_api_token" {
-  type = string
+resource "aws_security_group_rule" "restrict_inbound" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = var.allowed_cidr_blocks
+  security_group_id = var.security_group_id
 }
 
-variable "cloudflare_zone_id" {
-  type = string
+resource "aws_security_group_rule" "restrict_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = var.allowed_cidr_blocks
+  security_group_id = var.security_group_id
 }
 
-variable "cloudflare_record_name" {
-  type = string
-  default = "foobar" # TO CHANGE
+resource "aws_security_group" "update_existing" {
+  name        = "updated-default-sg"
+  description = "Updated security group with restricted access"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "UpdatedDefaultSecurityGroup"
+  }
+
+  # Remove all existing rules
+  ingress = []
+  egress  = []
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-variable "qovery_custom_domain" {
-  type = string
-  default = "foobar.meta.cloud" # TO CHANGE
+resource "aws_security_group_rule" "allow_internal_traffic" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.update_existing.id
+  security_group_id        = aws_security_group.update_existing.id
 }
+###
