6719337cef344029e86b08d83429721644c200ba
Subham Kundu <152807348+htcd-subham@users.noreply.github.com>
2025-04-09 17:13:42 +0530

    Update examples/deploy-a-lifecycle-job/main.tf

--- examples/deploy-a-lifecycle-job/main.tf
+++ examples/deploy-a-lifecycle-job/main.tf
@@ -1,13 +1,85 @@
+```
 terraform {
   required_providers {
     qovery = {
       source = "qovery/qovery"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "qovery" {
   token = var.qovery_access_token
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "vpc_id" {
+  description = "ID of the VPC"
+  type        = string
+  default     = "vpc-0ddd932ce1bd038f6"
+}
+
+variable "security_group_id" {
+  description = "ID of the security group to be updated"
+  type        = string
+  default     = "sg-08fe4af16da156c0e"
+}
+
+variable "allowed_cidr_blocks" {
+  description = "List of allowed CIDR blocks for inbound traffic"
+  type        = list(string)
+  default     = ["10.0.0.0/16"] # Replace with your actual allowed CIDR blocks
+}
+
+resource "aws_security_group_rule" "restrict_inbound" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = var.allowed_cidr_blocks
+  security_group_id = var.security_group_id
+}
+
+resource "aws_security_group_rule" "restrict_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = var.allowed_cidr_blocks
+  security_group_id = var.security_group_id
+}
+
+resource "aws_security_group" "update_existing" {
+  name        = "updated-default-sg"
+  description = "Updated security group with restricted access"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "UpdatedDefaultSecurityGroup"
+  }
+
+  # Remove all existing rules
+  ingress = []
+  egress  = []
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group_rule" "allow_internal_traffic" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.update_existing.id
+  security_group_id        = aws_security_group.update_existing.id
 }
 
 resource "qovery_aws_credentials" "my_aws_creds" {
@@ -156,3 +228,4 @@ resource "qovery_deployment" "prod_deployment" {
   environment_id = qovery_environment.production.id
   desired_state  = "RUNNING"
 }
+```
